e04cd17c7f16dc2a829aed9e1f87106e2a26a625
Amy Parker <enbyamy@gmail.com>
2021-03-20 20:01:15 -0700

    Test commit

---
+++ server/basic-flask/main.py
@@ -0,0 +1,19 @@
+# statdis: The Server Statistics Display System
+#
+# Copyright (C) 2021
+# Amy Parker. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; only version 2 is in compliance.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You can also contact the creator of this program for the address of
+# the Free Software Foundation to receive a copy of the License by mail.
+#
